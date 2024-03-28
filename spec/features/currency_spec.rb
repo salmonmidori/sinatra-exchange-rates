45ed4ba5ef8b7281672a20ebd8ef6fcbe04b52c0
Ben Purinton <ben.purinton@gmail.com>
2024-03-28 14:41:08 -0800

    specs fixed

--- spec/features/currency_spec.rb
+++ spec/features/currency_spec.rb
@@ -9,15 +9,14 @@ end
 describe "root URL" do
   it "lists each of the Currency Symbols from the API", points: 2 do
     visit "/"
-    expect(page).to have_content("AED")
-    expect(page).to have_content("AFN")
-    expect(page).to have_content("ALL")
-    expect(page).to have_content("AMD")
-    expect(page).to have_content("ANG")
-    expect(page).to have_content("AOA")
-    expect(page).to have_content("TMT")
-    expect(page).to_not have_content("BOB"),
-      "Expected not to find the symbol 'BOB', hardcoded into the HTML but found it anyway."
+    expect(page).to have_content("EUR")
+    expect(page).to have_content("USD")
+    expect(page).to have_content("JPY")
+    expect(page).to have_content("BGN")
+    expect(page).to have_content("CZK")
+    expect(page).to have_content("DKK")
+    expect(page).to_not have_content("GBP"),
+      "Expected not to find the symbol 'GBP', hardcoded into the HTML but found it anyway."
   end
 end
 
@@ -25,39 +24,38 @@ describe "root URL" do
   it "has a link to '/X', where 'X' is a currency symbol, for each currency symbol", points: 1 do
     visit "/"
 
-    expect(page).to have_tag("a", :with => { :href => "/AED" })
-    expect(page).to have_tag("a", :with => { :href => "/AFN" })
-    expect(page).to have_tag("a", :with => { :href => "/ALL" })
-    expect(page).to have_tag("a", :with => { :href => "/AMD" })
-    expect(page).to have_tag("a", :with => { :href => "/ANG" })
-    expect(page).to have_tag("a", :with => { :href => "/AOA" })
-    expect(page).to have_tag("a", :with => { :href => "/TMT" })
+    expect(page).to have_tag("a", :with => { :href => "/EUR" })
+    expect(page).to have_tag("a", :with => { :href => "/USD" })
+    expect(page).to have_tag("a", :with => { :href => "/JPY" })
+    expect(page).to have_tag("a", :with => { :href => "/BGN" })
+    expect(page).to have_tag("a", :with => { :href => "/CZK" })
+    expect(page).to have_tag("a", :with => { :href => "/DKK" })
   end
 end
 
 describe "/[CURRENCY SYMBOL]" do
   it "has a level one heading with the text 'Convert [CURRENCY SYMBOL]'", points: 1 do
-    visit "/CUP"
+    visit "/EUR"
 
-    expect(page).to have_tag("h1", text: /Convert\s+CUP/i)
+    expect(page).to have_tag("h1", text: /Convert\s+EUR/i)
   end
 end
 
 describe "/[CURRENCY SYMBOL]" do
   it "has a level one heading with the text 'Convert [CURRENCY SYMBOL]'", points: 1 do
-    visit "/SVC"
+    visit "/USD"
 
-    expect(page).to have_tag("h1", text: /Convert\s+SVC/i)
+    expect(page).to have_tag("h1", text: /Convert\s+USD/i)
 
-    visit "/WST"
+    visit "/JPY"
 
-    expect(page).to have_tag("h1", text: /Convert\s+WST/i)
+    expect(page).to have_tag("h1", text: /Convert\s+JPY/i)
   end
 end
 
 describe "/[CURRENCY SYMBOL]" do
   it "has a link with the text 'back' to root URL", points: 1 do
-    visit "/TMT"
+    visit "/CZK"
 
     expect(page).to have_tag("a", :with => { :href => "/" }, :text => /back/i),
       "Expected to find <a> tag with the text 'back' and an href='/', but did not find one."
@@ -66,45 +64,45 @@ end
 
 describe "/[CURRENCY SYMBOL]" do
   it "has a link with the text, 'Convert 1 X to Y...', to '/X/Y', for each currency symbol", points: 1 do
-    visit "/TMT"
+    visit "/CZK"
 
-    expect(page).to have_tag("a", :with => { :href => "/TMT/AMD" }, :text => /Convert\s+1\s+TMT\s+to\s+AMD/i),
-      "Expected page to have a link with the text, 'Convert 1 TMT to AMD...', with an href='/TMT/AMD', but did not find one."
+    expect(page).to have_tag("a", :with => { :href => "/CZK/EUR" }, :text => /Convert\s+1\s+CZK\s+to\s+EUR/i),
+      "Expected page to have a link with the text, 'Convert 1 CZK to EUR...', with an href='/CZK/EUR', but did not find one."
 
-    expect(page).to have_tag("a", :with => { :href => "/TMT/AOA" }, :text => /Convert\s+1\s+TMT\s+to\s+AOA/i),
-      "Expected page to have a link with the text, 'Convert 1 TMT to AOA...', with an href='/TMT/AOA', but did not find one."
+    expect(page).to have_tag("a", :with => { :href => "/CZK/BGN" }, :text => /Convert\s+1\s+CZK\s+to\s+BGN/i),
+      "Expected page to have a link with the text, 'Convert 1 CZK to BGN...', with an href='/CZK/BGN', but did not find one."
   end
 end
 
 describe "/[CURRENCY SYMBOL]/[CURRENCY SYMBOL]" do
   it "has a level one heading with the text 'Convert [CURRENCY SYMBOL] to [CURRENCY SYMBOL]'", points: 1 do
-    visit "/ANG/AOA"
+    visit "/USD/EUR"
 
-    expect(page).to have_tag("h1", text: /Convert\s+ANG\s+to\s+AOA/i)
+    expect(page).to have_tag("h1", text: /Convert\s+USD\s+to\s+EUR/i)
   end
 end
 
 describe "/[CURRENCY SYMBOL]/[CURRENCY SYMBOL]" do
   it "has the text '1 [CURRENCY SYMBOL] equals [CURRENCY SYMBOL]'", points: 1 do
-    visit "/CUP/SVC"
+    visit "/USD/EUR"
     p page.text
-    expect(page).to have_text(/1\s+CUP\s+equals\s+0.339787\s+SVC/i),
-      "Expected page to have text that follows the pattern, '1 CUP equals 0.339787 SVC', but it didn't."
+    expect(page).to have_text(/1\s+USD\s+equals\s+0.9247900944\s+EUR/i),
+      "Expected page to have text that follows the pattern, '1 USD equals 0.9247900944 EUR', but it didn't."
   end
 end
 
 describe "/[CURRENCY SYMBOL]/[CURRENCY SYMBOL]" do
   it "has a link with the text 'back' to '/[CURRENCY SYMBOL]'", points: 1 do
-    visit "/CUP/SVC"
+    visit "/USD/EUR"
 
-    expect(page).to have_tag("a", :with => { :href => "/CUP" }, :text => /back/i)
+    expect(page).to have_tag("a", :with => { :href => "/USD" }, :text => /back/i)
   end
 end
 
 describe "/[CURRENCY SYMBOL]/[CURRENCY SYMBOL]" do
   it "has a link with the text 'back' to '/[CURRENCY SYMBOL]'", points: 1 do
-    visit "/ANG/AOA"
+    visit "/USD/EUR"
 
-    expect(page).to have_tag("a", :with => { :href => "/ANG" }, :text => /back/i)
+    expect(page).to have_tag("a", :with => { :href => "/USD" }, :text => /back/i)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,42 +29,27 @@ path_to_file = Dir.pwd + "/spec/support/currency_symbols.json"
 api_response = open(path_to_file).read.chomp
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:get, /api.exchangerate.host\/list/).
+    stub_request(:get, /api.freecurrencyapi.com\/v1\/currencies/).
       with(headers: { "Accept" => "*/*", "User-Agent" => "Ruby" }).
       to_return(status: 200, body: api_response, headers: { "Content-Type" => "application/json" })
 
-    stub_request(:get, /api.exchangerate.host\/list/).
-      with(headers: { "Connection" => "close", "Host" => "api.exchangerate.host", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
-      to_return(status: 200, body: api_response, headers: { "Content-Type" => "application/json" })
-
-    stub_request(:get, /api.exchangerate.host\/live/).
-      with(headers: { "Accept" => "*/*", "User-Agent" => "Ruby" }).
-      to_return(status: 200, body: api_response, headers: { "Content-Type" => "application/json" })
-
-    stub_request(:get, /api.exchangerate.host\/live/).
-      with(headers: { "Connection" => "close", "Host" => "api.exchangerate.host", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
+    stub_request(:get, /api.freecurrencyapi.com\/v1\/currencies/).
+      with(headers: { "Connection" => "close", "Host" => "api.freecurrencyapi.com", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
       to_return(status: 200, body: api_response, headers: { "Content-Type" => "application/json" })
   end
 end
-path_to_file = Dir.pwd + "/spec/support/cup_to_svc.json"
-cup_to_svc = open(path_to_file).read.chomp
+
+path_to_file = Dir.pwd + "/spec/support/usd_to_eur.json"
+usd_to_eur = open(path_to_file).read.chomp
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:get, /api.exchangerate.host\/convert/).
+    stub_request(:get, /api.freecurrencyapi.com\/v1\/latest/).
       with(headers: { "Accept" => "*/*", "User-Agent" => "Ruby" }).
-      to_return(status: 200, body: cup_to_svc, headers: { "Content-Type" => "application/json" })
+      to_return(status: 200, body: usd_to_eur, headers: { "Content-Type" => "application/json" })
 
-    stub_request(:get, /api.exchangerate.host\/latest/).
-      with(headers: { "Accept" => "*/*", "User-Agent" => "Ruby" }).
-      to_return(status: 200, body: cup_to_svc, headers: { "Content-Type" => "application/json" })
-
-    stub_request(:get, /api.exchangerate.host\/convert/).
-      with(headers: { "Connection" => "close", "Host" => "api.exchangerate.host", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
-      to_return(status: 200, body: cup_to_svc, headers: { "Content-Type" => "application/json" })
-
-    stub_request(:get, /api.exchangerate.host\/latest/).
-      with(headers: { "Connection" => "close", "Host" => "api.exchangerate.host", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
-      to_return(status: 200, body: cup_to_svc, headers: { "Content-Type" => "application/json" })
+    stub_request(:get, /api.freecurrencyapi.com\/v1\/latest/).
+      with(headers: { "Connection" => "close", "Host" => "api.freecurrencyapi.com", "User-Agent" => "http.rb/#{HTTP::VERSION}" }).
+      to_return(status: 200, body: usd_to_eur, headers: { "Content-Type" => "application/json" })
   end
 end
 
