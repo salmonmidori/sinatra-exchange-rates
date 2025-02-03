1f096f8156a6e7fb940f163b2378ea79003b21c5
Amy May Nguyen <anguye10@uchicago.edu>
2025-02-03 00:01:09 +0000

    Adjusting code to make it agreeable with test expectations

--- app.rb
+++ app.rb
@@ -3,19 +3,22 @@ require "sinatra/reloader"
 require "dotenv/load"
 require "http"
 
-currlist_resp = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}").to_s
-parsed_currlist_resp = JSON.parse(currlist_resp)
-all_currencies = parsed_currlist_resp["currencies"].keys
+
 
 
 get("/") do
-  @all_currencies = all_currencies
+  @currlist_resp = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}").to_s
+  @parsed_currlist_resp = JSON.parse(@currlist_resp)
+  @all_currencies = @parsed_currlist_resp["currencies"]
   erb(:pick_from_currency)
 end
 
 get("/:from_currency") do
-  @all_currencies = all_currencies
   @from_curr = params["from_currency"]
+
+  @currlist_resp = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}").to_s
+  @parsed_currlist_resp = JSON.parse(@currlist_resp)
+  @all_currencies = @parsed_currlist_resp["currencies"]
   erb(:pick_to_currency)
 end
 
@@ -23,9 +26,9 @@ get("/:from_currency/:to_currency") do
   @from_curr = params["from_currency"]
   @to_curr = params["to_currency"]
 
-  results_resp = HTTP.get("https://api.exchangerate.host/convert?from=#{@from_curr}&to=#{@to_curr}&amount=1&access_key=#{ENV["EXCHANGE_RATE_KEY"]}").to_s
-  parsed_results_resp = JSON.parse(results_resp)
-  @result = parsed_results_resp["result"]
+  @results_resp = HTTP.get("https://api.exchangerate.host/convert?from=#{@from_curr}&to=#{@to_curr}&amount=1&access_key=#{ENV["EXCHANGE_RATE_KEY"]}").to_s
+  @parsed_results_resp = JSON.parse(@results_resp)
+  @result = @parsed_results_resp["result"]
   
   erb(:results)
 end
